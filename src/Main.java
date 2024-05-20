adf614eeae08c78494507714d2f9a00478d5e780
Antonioafj <antonioafj.edu@gmail.com>
2024-05-19 21:29:28 -0400

    finalizando

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -22,10 +21,39 @@ public class Main {
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descrição da mentoria java");
         mentoria.setData(LocalDate.now());
+//
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp java developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println("Conteudos Inscritos" + devCamila.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos" + devCamila.getConteudosConcluidos());
+        System.out.println("XP: " + devCamila.calcularToralXp());
+
+        System.out.println("\n---------------\n");
+
+        Dev devTony = new Dev();
+        devTony.setNome("Tony");
+        devTony.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devTony.getConteudosInscritos());
+        devTony.progredir();
+        devTony.progredir();
+        devTony.progredir();
+        System.out.println("Conteudos Inscritos" + devTony.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos" + devTony.getConteudosConcluidos());
+        System.out.println("XP: " + devTony.calcularToralXp());
     }
 }
